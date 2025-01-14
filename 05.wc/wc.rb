386ffffd0013cc9dcbd968a59bd282760b2f39cc
yuki <y09425717@gmail.com>
2025-01-14 16:12:59 +0900

    コードの簡略化、変数名の見直しをした。

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -3,58 +3,77 @@
 
 require 'optparse'
 
-SPACE = 8
+COLUMN_WIDTH = 8
 
-def count_words(lines)
-  lines.sum { |line| line.split.length }
+def read_input(filename)
+  filename.empty? ? $stdin.read : File.read(filename)
+end
+
+def build_file_stats(input_text)
+  {
+    lines: input_text.lines.count,
+    words: count_words(input_text.lines),
+    bytes: input_text.bytesize
+  }
+end
+
+def count_words(text_lines)
+  text_lines.sum { |line| line.split.length }
 end
 
 def format_with_padding(text)
-  width = SPACE - text.to_s.length
-  (' ' * width).to_s + text.to_s
+  width = COLUMN_WIDTH - text.to_s.length
+  (' ' * width) + text.to_s
+end
+
+def calculate_total(stats_list, stats_key)
+  stats_list.sum { |i| i[stats_key] }
+end
+
+def format_summary(file_stats, file_name, options)
+  summary_line = []
+  summary_line << format_with_padding(file_stats[:lines]) if options['l']
+  summary_line << format_with_padding(file_stats[:words]) if options['w']
+  summary_line << format_with_padding(file_stats[:bytes]) if options['c']
+  summary_line << " #{file_name}"
+  summary_line
+end
+
+def display_summaries(summary_lines)
+  summary_lines.each do |summary_line|
+    puts summary_line.join('')
+  end
 end
 
 options = ARGV.getopts('l', 'w', 'c')
-no_option = options.values.none?
-file_names = ARGV.empty? ? [''] : ARGV
 
-current_file_summary = []
-file_info_matrix = []
-
-total_line = 0
-total_word = 0
-total_byte = 0
-
-file_names.each_with_index do |file, _i|
-  file_text = file == '' ? $stdin.read : File.read(file)
-
-  line_count = file_text.lines.count
-  word_count = count_words(file_text.lines)
-  bytesize_count = file_text.bytesize
-
-  total_line += line_count
-  total_word += word_count
-  total_byte += bytesize_count
-
-  current_file_summary << format_with_padding(line_count) if options['l'] || no_option
-  current_file_summary << format_with_padding(word_count) if options['w'] || no_option
-  current_file_summary << format_with_padding(bytesize_count) if options['c'] || no_option
-  current_file_summary << " #{file}"
-
-  file_info_matrix << current_file_summary
-  current_file_summary = []
+if options.values.none?
+  options['l'] = true
+  options['w'] = true
+  options['c'] = true
 end
 
-file_info_matrix.each do |i|
-  puts i.join('')
+formatted_summaries = []
+file_stats_list = []
+
+file_names = ARGV.empty? ? [''] : ARGV
+
+file_names.each do |file|
+  file_text = read_input(file)
+  current_file_stats = build_file_stats(file_text)
+  summary_line = format_summary(current_file_stats, file, options)
+
+  formatted_summaries << summary_line
+  file_stats_list << current_file_stats
 end
 
 if file_names.size > 1
   totals = []
-  totals << format_with_padding(total_line) if options['l'] || no_option
-  totals << format_with_padding(total_word) if options['w'] || no_option
-  totals << format_with_padding(total_byte) if options['c'] || no_option
+  totals << format_with_padding(calculate_total(file_stats_list, :lines)) if options['l']
+  totals << format_with_padding(calculate_total(file_stats_list, :words)) if options['w']
+  totals << format_with_padding(calculate_total(file_stats_list, :bytes)) if options['c']
   totals << ' total'
-
-  puts totals.join
+  formatted_summaries << totals
 end
+
+display_summaries(formatted_summaries)
